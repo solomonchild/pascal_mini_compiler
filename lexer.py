2ff382eff04c53b667e5ec6562ea6a8f764f63fa
Vlad <solomonchild@gmail.com>
2014-12-05 21:53:34 +0200

    Finihsed

--- lexer.py
+++ lexer.py
@@ -13,7 +13,7 @@ class Token:
         self.table = table
 
     def __str__(self):
-        return "Kind: {0}, value: {1}, idx: {2}".format(self.kind, self.val, self.idx)
+        return "Kind: {0}, value: \"{1}\", idx: {2}".format(self.kind, self.val, self.idx)
 
 class IDEntry:
     def __str__(self):
@@ -25,7 +25,7 @@ class IDEntry:
 
 class LiteralEntry:
     def __str__(self):
-            return "Literal({0}), value: \"{1}\", index of id table: {2}".format(self.kind, self.val, self.idx)
+            return "Literal ({0}), value: \"{1}\"".format(self.kind, self.val)
 
     def __init__(self, kind, value):
         self.kind = kind
@@ -36,6 +36,9 @@ class KeywordTableEntry:
         self.val = val
         self.kind = kind
         self.isSep = isSep
+
+    def __str__(self):
+        return "Keyword ({0}), value: \"{1}\", is a separator: {2}".format(self.kind, self.val, self.isSep)
 
 class FileStream:
     def __init__(self, filename):
@@ -51,38 +54,43 @@ class FileStream:
 
     def get_char(self):
         char = None
-        if len(self.lines) == 0:
-            raise Exception("No lines")
+        #if len(self.lines) == 0:
+            #raise Exception("No lines")
+        if self.pos == len(self._get_current_line()) - 1 and self.cur_line == len(self.lines) - 1 and self.eof:
+            return None
 
-        if not self.eof:
-            char = self._get_current_line()[self.pos]
-
+        char = self._get_current_line()[self.pos]
         self.pos += 1
+
         if self.pos >= len(self._get_current_line()):
-            line = self.fd.readline()
-            line = line.rstrip("\n")
-            if line is "":
-                self.eof = True
-                return None
-            self.lines.append(line)
+            if self.cur_line == len(self.lines) -1:
+                line = self.fd.readline()
+                if line is "":
+                    self.eof = True
+                line = line.rstrip("\n")
+                if line is not "":
+                    self.lines.append(line)
             self.pos = 0
             self.cur_line += 1
+
         return char 
 
     def _get_current_line(self):
         return self.lines[self.cur_line]
 
     def put_char(self, how_many = 1):
-        if self.lines is []:
-            raise Exception("No lines")
-        elif self.pos == 0 :
-            if self.cur_line == 0:
-                raise Exception("Cannot put char: zeroeth position within only line")
+        while how_many > 0:
+            if self.lines is []:
+                raise Exception("No lines")
+            elif self.pos == 0 :
+                if self.cur_line == 0:
+                    raise Exception("Cannot put char: zeroeth position within only line")
+                else:
+                    self.cur_line -= 1
+                    self.pos = len(self._get_current_line()) - 1
             else:
-                self.cur_line -= 1
-                self.pos = len(self._get_current_line()) - 1
-        else:
-            self.pos -= 1
+                self.pos -= 1
+            how_many -= 1
         
 
     
@@ -112,14 +120,14 @@ class Lexer:
                 KeywordTableEntry("var", TokenType.VAR),
                 KeywordTableEntry("begin", TokenType.BEGIN),
                 KeywordTableEntry("end", TokenType.END),
-                KeywordTableEntry("(", TokenType.LPAREN, False),
-                KeywordTableEntry(")", TokenType.RPAREN, False),
-                KeywordTableEntry("[", TokenType.LSQUARE, False),
-                KeywordTableEntry("]", TokenType.RSQUARE, False),
-                KeywordTableEntry(",", TokenType.COMMA, False),
-                KeywordTableEntry(".", TokenType.DOT, False),
-                KeywordTableEntry(";", TokenType.SEMICOLON, False),
-                KeywordTableEntry(":", TokenType.COLON, False),
+                KeywordTableEntry("(", TokenType.LPAREN, True),
+                KeywordTableEntry(")", TokenType.RPAREN, True),
+                KeywordTableEntry("[", TokenType.LSQUARE, True),
+                KeywordTableEntry("]", TokenType.RSQUARE, True),
+                KeywordTableEntry(",", TokenType.COMMA, True),
+                KeywordTableEntry(".", TokenType.DOT, True),
+                KeywordTableEntry(";", TokenType.SEMICOLON, True),
+                KeywordTableEntry(":", TokenType.COLON, True),
                 KeywordTableEntry("+", TokenType.OPERATOR),
                 KeywordTableEntry("-", TokenType.OPERATOR),
                 KeywordTableEntry("\\", TokenType.OPERATOR),
@@ -154,7 +162,6 @@ class Lexer:
             #first check if exists in the keyword table
             if kwIndex is not None:
                 kwEntry = self.keywords[kwIndex]
-                if self.lexeme == ":=":
                 return Token(kwEntry.kind, self.lexeme, kwIndex, self.keywords)
             else:
                 #true ID, not a keyword
@@ -209,7 +216,8 @@ class Lexer:
                         return self.processLexeme(TokenType.REAL)
                     return self.processLexeme(TokenType.INTEGER)
                 else:
-                    self.stream.put_char()
+                    self.stream.put_char(2)
+                    char = self.stream.get_char()
                     self.lexeme = "" 
             elif char in string.digits:
                     while char and char in string.digits or (char is "." and "." not in self.lexeme):
@@ -231,7 +239,7 @@ class Lexer:
                 else:
                     self.stream.put_char()
                     return self.processLexeme()
-            if self.getIndexOfKw(char):
+            if self.getIndexOfKw(char) is not None:
                 self.lexeme += char
                 return self.processLexeme()
             else:
@@ -239,5 +247,10 @@ class Lexer:
                     self.lexeme += char
                     char = self.stream.get_char()
                 self.stream.put_char()
-                return Token(TokenType.UNKNOWN, self.lexeme, 0)
+                return Token(TokenType.UNKNOWN, self.lexeme, 0, None)
 
+    def printTable(self, table):
+        i = 0
+        for entry in table:
+            print("{0}) {1}".format(i, entry))
+            i += 1
--- main.py
+++ main.py
@@ -8,7 +8,19 @@ def main():
         line =  lexer.get_token()
         if line == None:
             break
+        elif line.kind == TokenType.UNKNOWN:
+            print("\nError: Unknown lexeme {0} at line {1}".format(line.val, lexer.stream.current_line_num()))
+            return
         print(line)
+
+    print("\nSuccess: got a stream of tokens")
+    
+    print("\nPrinting a table of keywords:\n")
+    lexer.printTable(lexer.keywords)
+    print("\nPrinting a table of literals:\n")
+    lexer.printTable(lexer.literals)
+    print("\nPrinting a table of IDs:\n")
+    lexer.printTable(lexer.identifiers)
     
 
 if __name__ == "__main__":
